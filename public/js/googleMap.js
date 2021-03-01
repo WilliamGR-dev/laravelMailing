ae48d3a1787d00e92689e7038bd543af0f9364ba
WilliamGR-dev <wgirardreydet@gmail.com>
2021-03-01 09:55:02 +0100

    HTTPS error

--- public/js/googleMap.js
+++ public/js/googleMap.js
@@ -1,12 +1,9 @@
 function initMap() {
-    // The location of Uluru
     const diteco = { lat: 48.9025994, lng: 2.4011896 };
-    // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 16,
         center: diteco,
     });
-    // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
         position: diteco,
         map: map,
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,19 @@
     <script async
             src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_KEY')}}&callback=initMap">
     </script>
+    <script>
+        function initMap() {
+            const diteco = { lat: 48.9025994, lng: 2.4011896 };
+            const map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 16,
+                center: diteco,
+            });
+            const marker = new google.maps.Marker({
+                position: diteco,
+                map: map,
+            });
+        }
+    </script>
 
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
